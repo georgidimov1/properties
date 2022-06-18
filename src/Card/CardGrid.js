a6ef2dfebc0561b22eb6e35bc833e20349ef6061
Georgi Dimov <g.dimov@gmail.com>
2022-06-18 18:30:45 +0300

    Update CardGrid.js

--- src/Card/CardGrid.js
+++ src/Card/CardGrid.js
@@ -15,21 +15,28 @@ function CardGrid (props){
         }
 
       },[])
-        return ( <span className='background'>
-{props.token!==null?
- <div className="cards">
-     <DataContext.Provider value={cardsAll}>
-        <Cards/>
-     </DataContext.Provider>
-</div>:
- <div className="page-wrapper bg-gra-01 p-t-180 p-b-100 font-poppins">
-    <blockquote className="welcome">
-        <p>There is only one happiness in this life, to love and be loved.</p>
-    </blockquote>
-    <footer className="author">George Sand</footer>
-</div>
-}    
-</span>  
+        return ( 
+        <div className="cards">
+            <DataContext.Provider value={cardsAll}>
+                <Cards/>
+            </DataContext.Provider>
+        </div>
+        // <span>
+        // {props.token!==null?
+        //     <div className="cards">
+        //         <DataContext.Provider value={cardsAll}>
+        //             <Cards/>
+        //         </DataContext.Provider>
+        //     </div>
+        //     :
+        //     <div className="page-wrapper bg-gra-01 p-t-180 p-b-100 font-poppins">
+        //         <blockquote className="welcome">
+        //             <p>There is only one happiness in this life, to love and be loved.</p>
+        //         </blockquote>
+        //         <footer className="author">George Sand</footer>
+        //     </div>
+        // }    
+        // </span>  
 
     )
 }
